950352afeaf1fac1acbfae27432d72f734b1e999
Michael O'Brien <michael@obrienlabs.org>
2020-02-17 16:30:15 -0500

    #1 base64 servlet filter using request attribute passing

--- reference-nbi/src/main/java/cloud/containerization/reference/nbi/ApiController.java
+++ reference-nbi/src/main/java/cloud/containerization/reference/nbi/ApiController.java
@@ -1,6 +1,7 @@
 package cloud.containerization.reference.nbi;
 
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -14,42 +15,33 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/api")
 // http://localhost:8080/nbi/api
 public class ApiController {
+	
+	private final static Logger LOG = Logger.getLogger(ApiController.class.getName());
 
     private final AtomicLong counter = new AtomicLong();
 	private static final CharSequence PASS = "PASS";
-	private static final CharSequence FAIL = "FAIL";
-    
-    /*@RequestMapping(method=RequestMethod.GET)
-    public @ResponseBody Api process(
-    		@RequestParam(value="action", required=true, defaultValue="undefined") String action) {
-    	String message = "pass";
-    	System.out.println(this.getClass().getCanonicalName());
-    	// external call
-    	//message = externalAPICall(action);
-    	Api api = new Api(counter.incrementAndGet(), message);
-    	return api;
-    } */
     
     @RequestMapping(method=RequestMethod.GET)
     public @ResponseBody Api process(
     		@RequestParam(value="action", required=true, defaultValue="undefined") String action,
-    		 HttpServletRequest request) {
-    	String message = counter.incrementAndGet() + " " + PASS.toString();
+    		HttpServletRequest request) {
+    	StringBuffer message = new StringBuffer(String.valueOf(counter.incrementAndGet()));
+    	// expecting non-base64 query string from the filter chain before this call
+    	String queryString = request.getQueryString();
+    	String decodedQueryString = (String)request.getAttribute("qs");
+    	LOG.info("queryString decoded: " + decodedQueryString);
     	
-    	message = message +  " " + 
-    			this.getClass().getCanonicalName() + 
-    			" remoteAddr: " +
-        	request.getRemoteAddr() + " localAddr: " + 
-    		request.getLocalAddr() + " remoteHost: " +
-        	request.getRemoteHost() + " serverName: " + 
-    		request.getServerName() + 
-    		" parameter::action=" + action;
-     	Api api = new Api(counter, message);
-     	
-    	System.out.println(this.getClass().getCanonicalName() + " " + message);// + " requestedSessionId: " + 
-        	//request.getRequestedSessionId() + " X-FORWARDED-FOR: " +
-    		//request.getHeader("X-FORWARDED-FOR"));
-
+    	message.append(" ").append(PASS.toString());
+    	message.append(" ").append(this.getClass().getCanonicalName())
+    			//.append(" remoteAddr: ").append(request.getRemoteAddr())
+    			//.append(" localAddr: ").append(request.getLocalAddr())
+    			//.append(" remoteHost: ").append(request.getRemoteHost())
+    			//.append(" serverName: ").append(request.getServerName())
+    			.append(" queryString: ").append(queryString)
+    			.append(" decodedQueryString: ").append(decodedQueryString);
+    	
+     	Api api = new Api(counter, message.toString());
+    	LOG.info(this.getClass().getCanonicalName() + " " + message);
     	return api;
     } 
     
--- reference-nbi/src/main/java/cloud/containerization/reference/nbi/Base64Filter.java
+++ reference-nbi/src/main/java/cloud/containerization/reference/nbi/Base64Filter.java
@@ -1,6 +1,7 @@
 package cloud.containerization.reference.nbi;
 
 import java.io.IOException;
+import java.util.Base64;
 import java.util.logging.Logger;
 
 import javax.servlet.Filter;
@@ -21,9 +22,29 @@ public class Base64Filter implements Filter {
 	public void doFilter(ServletRequest request, javax.servlet.ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		HttpServletRequest req = (HttpServletRequest) request;
-        LOG.info("Pre request: " + req.getRequestURI());
-  
+		// Note: the query string does not include the anchor hash (clent) content
+		String queryString = req.getQueryString();
+        LOG.info("Pre request + querystring: " + req.getRequestURI() + "?" + queryString);
+        // decode base64 encoded parameters high in the chain
+        String decoded = decode(queryString);
+        LOG.info("decoded: " + decoded);
+        
+        // 3 solutions to emulate a setQueryParameter() call (wrapping request, request attributes, or custom parameters)
+        req.setAttribute("qs", decoded);
         chain.doFilter(request, response);
         LOG.info("Post request: " + req.getRequestURI());
+	}
+	
+	
+	/**
+	 * execution=e1s1 = ZXhlY3V0aW9uPWUxczE= 
+	 */
+	private String encode(String url) {
+		return Base64.getUrlEncoder().encodeToString(url.getBytes());
+	}
+	
+	private String decode(String url) {
+		byte[] decodedBytes = Base64.getUrlDecoder().decode(url);
+		return new String(decodedBytes);
 	}
 }
