466ed7575923331922a6a131bba5228b5766e21a
Michael O'Brien <michael@obrienlabs.org>
2020-09-06 19:05:33 -0400

    #2 - add swagger2

--- reference-nbi/src/main/java/cloud/containerization/reference/nbi/Base64Filter.java
+++ reference-nbi/src/main/java/cloud/containerization/reference/nbi/Base64Filter.java
@@ -13,27 +13,32 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-@Component
-@Order(1)
+// disable for now
+//@Component
+//@Order(1)
 public class Base64Filter implements Filter {
 	private final static Logger LOG = Logger.getLogger(Base64Filter.class.getName());
+	public static final Boolean enabled = false;
 
 	@Override
 	public void doFilter(ServletRequest request, javax.servlet.ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		HttpServletRequest req = (HttpServletRequest) request;
-		// Note: the query string does not include the anchor hash (clent) content
-		String queryString = req.getQueryString();
-        LOG.info("Pre request + querystring: " + req.getRequestURI() + "?" + queryString);
-        //LOG.info(encode(queryString));
-        // decode base64 encoded parameters high in the chain
-        String decoded = decode(queryString);
-        LOG.info("decoded: " + decoded);
+		if(enabled) {
+			HttpServletRequest req = (HttpServletRequest) request;
+		
+			// Note: the query string does not include the anchor hash (clent) content
+			String queryString = req.getQueryString();
+			LOG.info("Pre request + querystring: " + req.getRequestURI() + "?" + queryString);
+			//LOG.info(encode(queryString));
+			// decode base64 encoded parameters high in the chain
+			String decoded = decode(queryString);
+			LOG.info("decoded: " + decoded);
         
-        // 3 solutions to emulate a setQueryParameter() call (wrapping request, request attributes, or custom parameters)
-        req.setAttribute("qs", decoded);
-        chain.doFilter(request, response);
-        LOG.info("Post request: " + req.getRequestURI());
+			// 3 solutions to emulate a setQueryParameter() call (wrapping request, request attributes, or custom parameters)
+			req.setAttribute("qs", decoded);
+			chain.doFilter(request, response);
+			LOG.info("Post request: " + req.getRequestURI());
+		}
 	}
 	
 	
