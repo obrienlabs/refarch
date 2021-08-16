31b470caad4f3bc228365fb9b2872c3a9cf5179a
Michael O'Brien <michael@obrienlabs.org>
2021-08-16 00:09:44 -0400

    #7 - add java 11 HttpClient

--- reference-aws-sam/PortalFunction/src/main/java/portal/App.java
+++ reference-aws-sam/PortalFunction/src/main/java/portal/App.java
@@ -3,7 +3,12 @@ package portal;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URI;
 import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -12,6 +17,7 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
 
 /**
  * Handler for requests to Lambda function.
@@ -27,8 +33,11 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
                 .withHeaders(headers);
         try {
             final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
-            String output = String.format("{ \"message\": \"hello world\", \"location\": \"%s\" }", pageContents);
+            String output = String.format("{ \"message\": \"hello world2\", \"location\": \"%s\" }", 
+            		pageContents);
 
+    		httpClient();
+ 
             return response
                     .withStatusCode(200)
                     .withBody(output);
@@ -39,6 +48,27 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
         }
     }
 
+    // from magellan/RestClient
+    private static final String URL_CREATE_RECORD =
+            "http://biometric.elasticbeanstalk.com/FrontController?action=latest&u=202107175";
+
+	private void httpClient() {
+		HttpClient httpClient = HttpClient.newBuilder().build();
+		HttpRequest request = HttpRequest.newBuilder()
+				.uri(URI.create(URL_CREATE_RECORD))
+				.GET()
+				.build();
+		try {
+			HttpResponse<String> response =
+				httpClient.send(request, BodyHandlers.ofString());
+			String body = response.body();
+			System.out.println("Response: " + body);
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		} catch (InterruptedException ie) {
+			ie.printStackTrace();
+		}
+	}
     private String getPageContents(String address) throws IOException{
         URL url = new URL(address);
         try(BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
